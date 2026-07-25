222e50d9e5c00ac131d046e79f4e723035a2af2c
Hareesh <coderhjs@gmail.com>
2026-07-25 22:21:59 +0530

    dataBase connectio

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//require ('dotenv').config({path:'/.env'})
+import dotenv from "dotenv"
+import express from "express";
+import connectDB from "./db/index.js";
+
+
+const app = express();
+dotenv.config({path:'/.env'})
+connectDB();
+
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         console.log("Database Connected");
+
+//         app.on("error", (error) => {
+//             console.error("App Error:", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server running on port ${process.env.PORT}`);
+//         });
+
+//     } catch (error) {
+//         console.error("Database Connection Error:", error);
+//     }
+// })();
